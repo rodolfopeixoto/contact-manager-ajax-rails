70d0562033df7f2464a511f9ebd740a830dfc206
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-01-31 21:51:56 -0200

    Added delete contact feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :contacts
   # get 'contacts/index', as: 'contacts'
   # get 'contacts/new', as: 'new_contact'
   # post 'contacts/create', as: 'create_contact'
   # get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
   # patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
+  # delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
   root 'contacts#index'
-  resources :contacts, except: [:show]
 end
