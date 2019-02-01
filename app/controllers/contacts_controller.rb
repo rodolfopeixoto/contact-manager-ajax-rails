9aa07070d92851105e0f5afea87a63a2987c95c6
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-01-31 22:12:44 -0200

    Fixed the selected group before/after saving using session

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,7 @@ class ContactsController < ApplicationController
   before_action :find_contact, only: %i[edit update destroy]
 
   def index
+    session[:selected_group_id] = params[:group_id]
     if params[:group_id] && !params[:group_id].empty?
       @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
     else
@@ -17,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = 'Contact was successfully created.'
-      redirect_to contacts_path
+      redirect_to contacts_path(previous_query_string)
     else
       render 'new'
     end
@@ -48,5 +49,9 @@ class ContactsController < ApplicationController
 
   def find_contact
     @contact = Contact.find(params[:id])
+  end
+
+  def previous_query_string
+    session[:selected_group_id] ? { group_id: session[:selected_group_id] } : {}
   end
 end
